a5e710d99c0f1016ae91ab56d5f846ea27f37ec1
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-07-01 15:58:05 +0100

    fix: muliple liking, failed queries

--- src/app/like-button.tsx
+++ src/app/like-button.tsx
@@ -8,34 +8,55 @@ import {
   HoverCardTrigger,
 } from "@/Components/ui/hover-card";
 import useSession from "@/hooks/useSession";
-import axios from 'axios'
-import {useQuery} from 'react-query'
+import axios, { AxiosError } from 'axios'
+import {useMutation, useQuery} from 'react-query'
+import { toast } from "@/Components/ui/use-toast";
 
 
 const LikeButton = ({ id, likes, likeCount }: { id: string, likes: string[], likeCount: number }) => {
   const user = useSession()
-  // const user = null
   const [likedByMe, setLikedByMe] = useState<boolean>(user ? likes.includes(user?._id) : false);
   const [likeNumber, setLikeNumber] = useState(likeCount)
-  async function likePost() {
-    setLikedByMe(!likedByMe)
-    setLikeNumber(likedByMe ? likeNumber - 1 : likeNumber + 1)
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/posts/${id}`,{
-      method: "PUT",
-      credentials: "include"
-    })
 
-  }
+  const {mutate: likePost, isLoading: isLiking} = useMutation({
+    mutationFn: async () => {
+        console.log({ likedByMe, likeNumber });
+      setLikeNumber(likedByMe ? likeNumber - 1 : likeNumber + 1)
+      setLikedByMe(!likedByMe)
+         await axios.put(
+           `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/posts/${id}`,
+           null,
+           {
+             withCredentials: true,
+           }
+      );
+    },
+    onSettled(data, error, variables, context) {
+      if (error) {
+        if (axios.isAxiosError(error)) {
+          toast({
+            description: `${error.response?.data.message}`,
+            variant: 'destructive'
+          })
+        }
+        setLikeNumber(likeNumber);
+        setLikedByMe(likedByMe)
+      }
+    },
+  });
 
   return (
       <div className="flex gap-2 items-center">
-      {user ? <HeartWithUser likePost={likePost} likedByMe={likedByMe} />: <HeartWithoutUser />}
+      {user ? <HeartWithUser isLiking={isLiking} likePost={likePost} likedByMe={likedByMe} />: <HeartWithoutUser />}
     <p>{likeNumber}</p>
       </div>
   )
 };
 
-function HeartWithUser({ likePost, likedByMe }: { likePost: () => void, likedByMe: boolean}) {
+function HeartWithUser({ isLiking, likePost, likedByMe }: { likePost: () => void, likedByMe: boolean, isLiking: boolean }) {
+  if (isLiking) likePost = () => {
+    return
+  }
   return <Heart className={"hover:fill-rose-400"} color={"black"} fill={ likedByMe ? "red" : 'none' } onClick={likePost}/>
 }
 
