a5bb9d29e9e3e270b17e9d314487fb207cfcd6ef
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-07-06 18:39:27 +0100

    complete styling of single post page

--- src/app/posts/CommentSection.tsx
+++ src/app/posts/CommentSection.tsx
@@ -8,11 +8,11 @@ import defaultImg from "../../../public/default.svg";
 
 function CommentSection() {
   return (
-    <>
-      <h3>Comments (3)</h3>
+    <div className="">
+      <h3 className="font-semibold text-xl">Comments (3)</h3>
       <div className="content divide-y-2">
-        <div className="grid">
-          <div className="flex gap-4 items-center">
+        <div className="grid gap-4 my-6">
+          <div className="flex gap-2 items-center">
             <Image
               src={defaultImg}
               width={24}
@@ -20,9 +20,12 @@ function CommentSection() {
               className="rounded-full object-cover h-8 w-8"
               alt=""
             />
-            Complexlity
+            <span className="font-bold">Complexlity</span>
           </div>
-          <Textarea placeholder="Write a thoughful comment" />
+          <Textarea
+            className="border-none focus-visible:ring-0 focus-visible:ring-offset-0 text-lg"
+            placeholder="Write a thoughful comment"
+          />
           <Button className="justify-self-end">Comment</Button>
         </div>
 
@@ -30,19 +33,19 @@ function CommentSection() {
           <div className="dropdown dropdown-bottom p-0">
             <label
               tabIndex={0}
-              className="flex px-1 py-1 items-center m-1 w-36 cursor-pointer rounded-md hover:slate-100 border-slate-400 border-2 gap-2"
+              className="flex px-1 py-1 items-center  w-36 cursor-pointer rounded-md hover:slate-100 border-slate-400 border-2 gap-2"
             >
               Top Comments
               <ChevronDown />
             </label>
             <ul
               tabIndex={0}
-              className="dropdown-content z-[1] divide-y-2 shadow  -mr-2 w-36 rounded-md"
+              className="dropdown-content z-[1] divide-y-2 shadow  -mr-2 w-36 rounded-lg overflow-hidden"
             >
-              <li className=" grid items-center hover:bg-slate-200 cursor-pointer px-4 py-2">
+              <li className=" grid items-center hover:bg-slate-200 cursor-pointer px-4 py-2 bg-white ">
                 Top Comments
               </li>
-              <li className="grid items-center hover:bg-slate-200 cursor-pointer px-4 py-2">
+              <li className="grid items-center hover:bg-slate-200 cursor-pointer px-4 py-2  bg-white ">
                 New Comments
               </li>
             </ul>
@@ -53,28 +56,38 @@ function CommentSection() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
 function SingleComment() {
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 my-4">
       <div className="profile flex items-center gap-1">
-        <Image src={defaultImg} alt="" width={12} height={12} className="object-cover w-8 h-8 rounded-full" />
+        <Image
+          src={defaultImg}
+          alt=""
+          width={12}
+          height={12}
+          className="object-cover w-8 h-8 rounded-full"
+        />
         <div>
           <p className="font-bold">Complexlity</p>
           <span>Jul 5</span>
         </div>
       </div>
       <div className="prose">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id velit vitae quaerat.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id velit vitae
+        quaerat.
       </div>
-      <div className="flex gap-1">
-        <Heart /> 1
+      <div className="flex items-center">
+        <div className="rounded-full p-2 hover:bg-rose-100 hover:text-rose-600">
+          <Heart size={23} />
+        </div>
+        <span>30</span>
       </div>
     </div>
-  )
+  );
 }
 
 
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -13,9 +13,11 @@ import CommentSection from "./CommentSection";
 const SinglePost = () => {
   return (
     <div className="container">
-      <header className="grid justify-center text-center">
-        <h1>What in the world is working will still work again</h1>
-        <p className="flex items-center gap-4 not-prose">
+      <header className=" grid justify-center text-center py-8">
+        <h1 className="font-extrabold text-3xl md:text-4xl mb-4">
+          What in the world is working will still work again
+        </h1>
+        <p className="mx-auto flex items-center gap-2 md:text-xl mb-4">
           <Image
             src={defaultImg}
             width={24}
@@ -23,7 +25,7 @@ const SinglePost = () => {
             className="rounded-full object-cover h-8 w-8"
             alt=""
           />
-          <span>Complexlity</span>
+          <span className="font-bold">Complexlity</span>
           <span className="inline-block w-1.5 h-1.5 rounded-full bg-gray-600 align-middle"></span>
           <span>Jul 5, 2023</span>
           <span className="inline-block w-1.5 h-1.5 rounded-full bg-gray-600 align-middle"></span>
@@ -74,12 +76,12 @@ const SinglePost = () => {
             </HoverCardContent>
           </HoverCard>
         </div>
-        <div className="bg-orange-400 w-[400px] h-[400px] mx-auto my-8">
+        <div className="h-[400px] mx-auto bg-orange-400 w-full m-10">
           <Image
             src={defaultImg}
             width={24}
             height={24}
-            className="rounded-full w-full h-full object-cover"
+            className="w-full h-full object-cover"
             alt=""
           />
         </div>
@@ -122,14 +124,18 @@ const SinglePost = () => {
           eligendi porro et, a recusandae excepturi rem tempore explicabo eaque
           itaque perferendis ipsum, hic esse dignissimos dolorum! Nobis.
         </p>
+        {/* Sticky Buttons */}
         <div className="not-prose sticky bottom-10 bg-white rounded-full items-center flex max-w-fit px-5 py-1 text-sm border-2 border-slate-200 mx-auto">
           <div className="flex gap-1 items-center">
-            <div className="rounded-full p-2 hover:bg-gray-200">
+            <div className="rounded-full p-2 hover:bg-rose-100 hover:text-rose-600">
               <Heart size={23} />
             </div>
             <span>30</span>
           </div>
+
           <div className="w-0.5 h-6 mx-2 bg-slate-200"></div>
+
+          {/* Comments Drawer */}
           <div className="">
             <div className="drawer drawer-end">
               <input
@@ -142,17 +148,16 @@ const SinglePost = () => {
                   htmlFor="my-drawer-4"
                   className="flex gap-1 items-center"
                 >
-                  <div className="rounded-full p-2 hover:bg-gray-200">
+                  <div className="rounded-full p-2 hover:bg-sky-100 hover:text-sky-600">
                     <MessagesSquare size={23} />
                   </div>
                   <span>30</span>
                 </label>
               </div>
-              <div className="drawer-side">
+              <div className="drawer-side ">
                 <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
-                <div className="menu p-4 z-[40] relative w-1/2 h-full bg-base-200 text-base-content">
-                  {/* Drawer Content */}
-                <CommentSection />
+                <div className="menu p-4 z-[40] relative w-1/2 h-full bg-white text-base-content">
+                  <CommentSection />
                 </div>
               </div>
             </div>
