346f531f4c3ae435bb4478688d7a25625c7ef943
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-07-08 22:31:34 +0100

    change posts route to client

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -4,11 +4,13 @@ import { usePostsContext } from "@/contexts/PostsProvider"
 import SinglePost from "./SinglePost"
 // import { getSinglePost } from '@/lib/serverFunctions'
 import {redirect} from 'next/navigation'
+import usePosts from "@/hooks/usePosts"
 
 export default async function Post({params}: {params: {id: string}}) {
   const posts = usePosts()
-  // const post = await getSinglePost(params.id)
-  // if(!post) redirect('/')
+  if(!posts) redirect('/')
+  const post = posts.filter(post => post._id === params.id)[0]
+  if(!post) redirect('/')
   return (
     <SinglePost post={post} />
   )
