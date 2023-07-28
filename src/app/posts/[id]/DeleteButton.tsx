247624451399dc213eb9aac4764e0ec13d4ac8f2
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-07-28 22:34:49 +0100

    complete comment deletion

---
+++ src/app/posts/[id]/DeleteButton.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import { Trash2 } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { useMutation } from "react-query";
+import axios from "axios";
+import { toast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+
+
+
+const DeleteButton = ({ id }: { id: string }) => {
+  const router = useRouter()
+
+
+  const { mutate: deleteComment, isLoading: deleting } = useMutation({
+    // @ts-ignore
+    mutationFn: async (id: string) => {
+      await axios.delete(
+        `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/comments/${id}`,
+        {
+          withCredentials: true,
+        }
+      );
+    },
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        toast({
+          description: `${error.response?.data.message}`,
+          variant: "destructive",
+        });
+      }
+      return toast({
+        title: "Something went wrong",
+        variant: "destructive",
+      });
+    },
+    onSuccess() {
+      toast({
+        title: "Comment deleted successfully"
+      })
+      router.refresh();
+    },
+  });
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Trash2 className="cursor-pointer hover:text-red-800" />
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>So you want to uncomment now?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone!!. Always think before you speak (or
+            comment)
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction className="p-0">
+            <Button
+              isLoading={deleting}
+              variant={"destructive"}
+              onClick={() => deleteComment(id)}
+            >
+              Delete
+            </Button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
+
+export default DeleteButton;
--- src/app/posts/[id]/SingleComment.tsx
+++ src/app/posts/[id]/SingleComment.tsx
@@ -1,10 +1,15 @@
+'use client'
+
 import { OmittedComment } from "@/lib/types";
 import { formatDate } from "@/lib/utils";
 import LikeButton from "./LikeButton";
 import Image from "next/image";
 import defaultImg from "../../../../public/default.svg";
+import useSession from "@/hooks/useSession";
+import DeleteButton from "./DeleteButton";
 
 function SingleComment({ comment }: { comment: OmittedComment }) {
+const user = useSession()
   return (
     <div className="space-y-4  py-4">
       <div className="profile flex items-center gap-1">
@@ -22,13 +27,17 @@ function SingleComment({ comment }: { comment: OmittedComment }) {
       </div>
       <div className="prose">{comment.comment}</div>
 
-      <div className="flex items-center">
+      <div className="flex items-center justify-between">
         <LikeButton
           id={comment._id}
           likes={comment.likes}
           likeCount={comment.likeCount}
           type="comments"
         />
+        {user?._id === comment.user._id ? <DeleteButton
+          id={comment._id}
+        /> : null}
+
       </div>
     </div>
   );
