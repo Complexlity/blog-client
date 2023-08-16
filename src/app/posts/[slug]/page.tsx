b7d97e46c354dc86930d8357924b9b658f456c59
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-08-16 01:21:15 +0100

    feat: generate post metadata

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,22 +1,34 @@
 import { getSinglePost } from "@/lib/serverFunctions";
+import type { Metadata } from "next";
 import { redirect } from "next/navigation";
 import SinglePost from "./SinglePost";
-
 
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
+type Props = {
+  params: { slug: string };
+};
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // read route params
+  const slug = params.slug;
+  const post = await getSinglePost(slug);
+  return {
+    title: `${post?.title} | Complexlity's Blog`,
+    openGraph: {
+      images: [post?.coverImageSource!],
+    },
+  };
+}
 
 export default async function Post({ params }: { params: { slug: string } }) {
   const post = await getSinglePost(params.slug);
+  
   if (!post) redirect("/");
   return (
-
     <>
-
-  <SinglePost post={post}
-  />;
-  </>
-  )
+      <SinglePost post={post}  />;
+    </>
+  );
 }
