2a830fbd085299bd158d137986e82e3a5e1ff793
Complexlity <nwalozielijah@gmail.com>
2024-05-28 21:01:13 +0100

    chore: temp auth

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,3 +1,4 @@
+import { getUser } from "@/lib/serverFunctions";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
 const f = createUploadthing();
@@ -11,13 +12,17 @@ export const ourFileRouter = {
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
-      const user = await auth(req);
+
+      //TODO: Use auth here
+      // const user = await getUser()
+      
+      const user = await auth(req)
 
       // If you throw, the user will not be able to upload
       if (!user) throw new Error("Unauthorized");
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
+      return {}
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // console.log("Upload complete for userId:", metadata.userId);
